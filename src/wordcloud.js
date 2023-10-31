01a6182296f8ec8dbb070ae370d0a156526286cf
tkming0916 <iguchi.takumi@g.wakayama-u.jp>
2023-10-31 14:44:21 +0900

    ワードクラウド作成用にブランチ切った

---
+++ src/wordcloud.js
@@ -0,0 +1,22 @@
+const wordcloud_json = [
+  { word: "今日", count: 3 },
+  { word: "晴れ", count: 1 },
+  { word: "領域", count: 1 },
+  { word: "展開", count: 1 },
+  { word: "怖い", count: 1 },
+  { word: "ねむたい", count: 1 },
+  { word: "交流", count: 2 },
+  { word: "会", count: 2 },
+  { word: "あり", count: 2 },
+  { word: "開発", count: 2 },
+  { word: "楽しい", count: 2 },
+  { word: "寿司", count: 1 },
+  { word: "食べ", count: 2 },
+  { word: "ラーメン", count: 1 },
+  { word: "花見", count: 1 },
+  { word: "し", count: 1 },
+  { word: "富士山", count: 1 },
+  { word: "見て", count: 1 },
+];
+// これでJSON形式を宣言できているか不明．多分似た形式になってるだけで宣言できてないっぽい．
+// wordcloudにはこの形式でデータを渡す必要がある．
