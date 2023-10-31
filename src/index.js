e7403a0717aa68791da7f322e70f21d8c2107f6d
tkming0916 <iguchi.takumi@g.wakayama-u.jp>
2023-10-31 14:32:28 +0900

    firebaseから全期間メッセージを取得． その後形態素解析し，名詞と動詞と形容詞だけ抽出． 単語を頻度計算しJSON形式で出力

--- src/index.js
+++ src/index.js
@@ -35,17 +35,49 @@ async function handleEvent(event) {
   insertData(userId,text); // database.jsのgetData関数を呼び出す
 
   const getDataResult = await getData(userId); // database.jsのgetData関数を呼び出す
+  // 型はlistぽい
 
-  words = await getAnalyzedWord("みなりかけるは天才だ！")
+  // words = await getAnalyzedWord("みなりかけるは天才だ！")
+  words = await getAnalyzedWord(getDataResult.join("."))
     .then((response) => {
       return JSON.parse(response);
     })
     .catch((error) => {
       console.error(error);
     }); // analysiswords.jsのanalyzeWords関数を呼び出す
-  console.log(words.result.tokens);
 
-  return client.replyMessage(event.replyToken, mes);
+  // console.log(words.result.tokens);
+  words = words.result.tokens; // 形態素解析の結果を取得
+  let myDictionary = {}; // 単語の辞書
+  for (var i = 0; i < words.length; i++) {
+    // console.log(words[i][3]);
+    if (words[i][3] == "名詞" || words[i][3] == "動詞" || words[i][3] == "形容詞") { // 品詞でフィルタリング
+      // console.log(words[i][0]);
+      if (myDictionary[words[i][0]] == null) { // 単語が初登場なら辞書に追加
+        myDictionary[words[i][0]] = 1;
+      } else {
+        myDictionary[words[i][0]] += 1;
+      }
+    }
+  }
+  // console.log("=========================================================================");
+  // console.log(myDictionary);
+  // console.log("=========================================================================");
+
+  var outputArray = [];
+  for (var key in myDictionary) {
+    if (myDictionary.hasOwnProperty(key)) {
+      var newItem = {
+        word: key,
+        count: myDictionary[key],
+      };
+      outputArray.push(newItem);
+    }
+  }
+  console.log(JSON.stringify(outputArray)); // JSON形式で出力（D3-cloudに必要な形式）
+
+
+  return client.replyMessage(event.replyToken, mes); // オウム返し
 }
 
 app.listen(PORT);
