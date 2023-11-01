7a64dbe691cb66bad719a920927b3dc26eecd5c2
minaremote <s246276@wakayama-u.ac.jp>
2023-11-01 15:40:51 +0900

    リファクタ

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
-
+const { makeReply } = require("./makeReply.js");// 返信生成用の関数を読み込む
 const express = require("express");
 const line = require("@line/bot-sdk");
 const PORT = process.env.EXPRESS_PORT;
-const {insertData,getData} = require("./database.js"); // データベース関連の関数をdatabase.jsから読み込む
-const { getAnalyzedWord } = require("./analysiswords.js"); // 形態素解析関連の関数をanalysiswords.jsから読み込む
 
+// env呼び出し
 const config = {
   channelSecret: process.env.LINE_CHANNEL_SECRET,
   channelAccessToken: process.env.LINE_ACCESS_TOKEN
 };
 
+// インスタンス生成
 const app = express();
+const client = new line.Client(config);
 
+// ExpressアプリケーションのPOSTルート "/webhook" に対するハンドラ関数
 app.post("/webhook", line.middleware(config), (req, res) => {
   console.log(req.body.events);
 
@@ -21,32 +23,25 @@ app.post("/webhook", line.middleware(config), (req, res) => {
   );
 });
 
-const client = new line.Client(config);
-
+// イベントの処理　今はテキストにのみ反応
 async function handleEvent(event) {
+  // メッセージにのみ返信　余裕があれば他もやる
   if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
+  
+  // 返信用メッセージの生成
+  mes = makeReply(event);
 
-  let mes = { type: "text", text: event.message.text };
-  const userId = event.source.userId; // LINEのユーザーID
-  const text = event.message.text; // ユーザーが送信したテキスト
+  // メッセージが空の場合は返信無し
+  if (mes == null) {
+    return Promise.resolve(null);
+  }
 
-  insertData(userId,text); // database.jsのgetData関数を呼び出す
-
-  const getDataResult = await getData(userId); // database.jsのgetData関数を呼び出す
-
-  words = await getAnalyzedWord("みなりかけるは天才だ！")
-    .then((response) => {
-      return JSON.parse(response);
-    })
-    .catch((error) => {
-      console.error(error);
-    }); // analysiswords.jsのanalyzeWords関数を呼び出す
-  console.log(words.result.tokens);
-
+  // 返信
   return client.replyMessage(event.replyToken, mes);
 }
 
+// 指定のポートで起動
 app.listen(PORT);
-console.log(`Server running at ${PORT}`);
+// console.log(`Server running at ${PORT}`);
