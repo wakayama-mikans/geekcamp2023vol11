e860a60f4c9e606728c2c5a8d20e862a1c06fe05
kakeru515 <s246276@wakayama-u.ac.jp>
2023-11-02 19:27:49 +0900

    日数を指定した際の返答処理を実装

--- src/makeReply.js
+++ src/makeReply.js
@@ -1,18 +1,20 @@
-const { insertData , getData } = require("./database.js"); // データベース関連の関数をdatabase.jsから読み込む
+const { insertData, getLatestTopic } = require("./database.js"); // データベース関連の関数をdatabase.jsから読み込む
 const { otherOpinions } = require("./flexmessages/sample.js")
 
 // ユーザーごとの状態を管理するオブジェクト
 const userStates = {};
 
-function makeReply (event) {
+async function makeReply (event) {
   const userId = event.source.userId; // LINEのユーザーID
   const text = event.message.text; // ユーザーが送信したテキスト
   let mes;
 
   if (text === "ジャーナルスタート！") {
     // ジャーナルの支援をリクエストした場合、状態を初期化
-    // userStates[userId] = "start";
-    userStates[userId] = "finish";
+    userStates[userId] = "start";
+
+    // デバッグ用
+    // userStates[userId] = "finish";
 
     // メッセージリストからランダムに1つを選択
     const initialMessages = ["将来は何になりたいですか？", "何かしてみたいことはありますか？"];
@@ -23,21 +25,48 @@ function makeReply (event) {
     ];
     // ユーザーに複数のメッセージを送信
     mes = responseMessages.map(text => ({ type: "text", text }));
-  } else if (text === "支援を終了します") {
-    // 支援を終了した場合、状態を初期化
+
+  } else if (text === "一日の結果を見せて！") {
     userStates[userId] = "finish";
-    mes = { type: "text", text: "支援を終了しました" };
+    // 1日分のワードクラウドを作成
+    mes = { type: "text", text: "一日分の結果です！🥳" };
+
+  } else if (text === "一週間の結果を見せて！") {
+    userStates[userId] = "finish";
+    // 7日分のワードクラウドを作成
+    mes = { type: "text", text: "一週間分の結果です！🥳" };
+
+  } else if (text === "一ヶ月の結果を見せて！") {
+    userStates[userId] = "finish";
+    // 30日分のワードクラウドを作成
+    mes = { type: "text", text: "一ヶ月の結果です！🥳" };
+
   } else {
 
+    if (text !== "はい" && text !== "いいえ") {
+      // id, status, textをDBに格納
+      insertData(userId, userStates[userId], text);
+    }
+
+    // ステータスに応じて挙動を決める
     switch (userStates[userId]) {
       case "start":
         // 最初のやり取り
-        // const initialMessages = ["それはどうして？", "その方法は？"];
-        const initialMessages = ["もっと具体的に言うと？"];
-        const randomIndex = Math.floor(Math.random() * initialMessages.length);
-        mes = { type: "text", text: initialMessages[randomIndex] };
-        userStates[userId] = "topic"; // statusを"topic"として設定
-        console.log("topicに変更");
+        if (text === "いいえ") {
+          const finishMassages = [
+            "サポートはこれにて終了です！",
+            "お疲れさまでした！🫠"
+          ]
+          mes = finishMassages.map(text => ({ type: "text", text }));
+          userStates[userId] = "exception"; // statusを"exception"として設定
+          console.log("exceptionに変更");
+        } else {
+          const initialMessages = ["もっと具体的に言うと？"];
+          const randomIndex = Math.floor(Math.random() * initialMessages.length);
+          mes = { type: "text", text: initialMessages[randomIndex] };
+          userStates[userId] = "topic"; // statusを"topic"として設定
+          console.log("topicに変更");
+        }
         break;
 
       case "topic":
@@ -61,11 +90,14 @@ function makeReply (event) {
         // const remainingMessagesWhy = whyMessages.filter(message => message !== userStates[userId].lastMessage);
         // 残りのメッセージからランダムに選択
         const randomIndexWhy = Math.floor(Math.random() * whyMessages.length);
-        mes = { type: "text", text: whyMessages[randomIndexWhy] };
         if (randomIndexWhy === 0) {
+          mes = { type: "text", text: whyMessages[randomIndexWhy] };
           userStates[userId] = "finish";
           console.log("finishに変更");
         } else {
+          const latestTopic = await getLatestTopic(userId);
+          console.log(latestTopic);
+          mes = { type: "flex", altText: "他の選択肢について考えてみよう！😎", contents: otherOpinions(latestTopic) };
           userStates[userId] = "start";
           console.log("startに変更");
         }
@@ -73,38 +105,39 @@ function makeReply (event) {
 
       case "how":
         // 3回目のやり取り 3つの質問から使ってないものを選択
-        const howMessages = ["どうしてそう考えたの？", "他の選択肢はある？"];
+        const howMessages = ["どうしてそう考えたの？😗", "他の選択肢はある？😗"];
         // const remainingMessagesHow = howMessages.filter(message => message !== userStates[userId].lastMessage);
         // 残りのメッセージからランダムに選択
         const randomIndexHow = Math.floor(Math.random() * howMessages.length);
-        mes = { type: "text", text: howMessages[randomIndexHow] };
         if (randomIndexHow === 0) {
+          mes = { type: "text", text: howMessages[randomIndexHow] };
           userStates[userId] = "finish";
           console.log("finishに変更");
         } else {
+          const latestTopic = await getLatestTopic(userId);
+          console.log(latestTopic);
+          mes = { type: "flex", altText: "他の選択肢について考えてみよう！😎", contents: otherOpinions(latestTopic) };
           userStates[userId] = "start";
           console.log("startに変更");
         }
         break;
     
       case "finish":
-        // const finishMassages = [
-        //   "支援はこれにて終了です",
-        //   "お疲れさまでした！"
-        // ]
-        // mes = finishMassages.map(text => ({ type: "text", text }));
+        const finishMassages = [
+          "サポートはこれにて終了です",
+          "お疲れさまでした！🫠"
+        ]
+        mes = finishMassages.map(text => ({ type: "text", text }));
+        // const latestTopic = await getLatestTopic(userId);
+        // console.log(latestTopic);
+        // mes = { type: "flex", altText: "他の選択肢について考えてみよう！😎", contents: otherOpinions(latestTopic) };
         // console.log("nullを返信");
-        mes = { type: "flex", altText: "他の選択肢", contents: otherOpinions("お金持ちになりたい！") };
-        // mes = [otherOpinions("お金持ちになりたい！")];
         break;
     
       default:
         mes = null;
         userStates[userId] = "Not supported"
     }
-    
-    // id, status, textをDBに格納
-    insertData(userId, userStates[userId], text);
   }
 
   console.log(userStates[userId])
