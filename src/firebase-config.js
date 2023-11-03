b09ce38d58b0b6b43c471ca2b70bc04d7f7b2bde
tagahei <tagahei0505@gmail.com>
2023-11-03 10:17:39 +0900

    無駄なもの削除

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,31 +1,18 @@
 // Firebase用の設定
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-const {
-    initializeApp,
-    applicationDefault,
-    cert,
-  } = require("firebase-admin/app");
-  const {
-    getFirestore,
-    Timestamp,
-    FieldValue,
-    Filter,
-    serverTimestamp,
-  } = require("firebase-admin/firestore");
-  const { getStorage, getDownloadURL } = require("firebase-admin/storage");
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const { getStorage, getDownloadURL } = require("firebase-admin/storage");
+const serviceAccount = require("../serviceAccountKey.json");
 
-  const serviceAccount = require("../serviceAccountKey.json");
-  
-  initializeApp({
-    credential: cert(serviceAccount),
-    storageBucket: 'gs://geekcamp2023vol11.appspot.com'
-  });
-  
-  const bucket = admin.storage().bucket();
-  const db = getFirestore();
-  const storage = getStorage();
-  const ref = storage.ref
+initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: "gs://geekcamp2023vol11.appspot.com",
+});
 
+const bucket = admin.storage().bucket();
+const db = getFirestore();
+const storage = getStorage();
 
-module.exports = { db ,storage, bucket, getDownloadURL};
+module.exports = { db, storage, bucket, getDownloadURL };
