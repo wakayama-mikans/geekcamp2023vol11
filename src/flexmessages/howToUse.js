487317ae7428ade9918041aea96b096eeac471f5
tkming0916 <iguchi.takumi@g.wakayama-u.jp>
2023-11-04 16:04:08 +0900

    画像名変更

--- src/flexmessages/howToUse.js
+++ src/flexmessages/howToUse.js
@@ -1,4 +1,4 @@
-function howToUseing(){
+function howToUseing() {
   return {
     type: "carousel",
     contents: [
@@ -10,7 +10,7 @@ function howToUseing(){
           contents: [
             {
               type: "image",
-              url: "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2F1.png?alt=media&token=4231cd2d-4226-4684-9db0-e9a8bae6f5f2",
+              url: "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2F1.png?alt=media&token=8697ee98-9315-4fcf-b1ef-4609e88e9e75",
               size: "full",
               aspectMode: "cover",
               aspectRatio: "1:1",
@@ -28,7 +28,7 @@ function howToUseing(){
           contents: [
             {
               type: "image",
-              url: "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2F2.png?alt=media&token=a405d5ae-4ffd-4388-8732-651b0b6a2723",
+              url: "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2F2.png?alt=media&token=d3a8c160-b22b-4e51-8987-214d2c35ef4f",
               size: "full",
               aspectMode: "cover",
               aspectRatio: "1:1",
@@ -46,7 +46,7 @@ function howToUseing(){
           contents: [
             {
               type: "image",
-              url: "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2F3.png?alt=media&token=a9fac52a-8af6-4392-81ff-cd32cbaafad0",
+              url: "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2F3.png?alt=media&token=b5dc0203-8bd9-483f-894b-f10ccf567509",
               size: "full",
               aspectMode: "cover",
               aspectRatio: "1:1",
