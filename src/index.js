0b997ab002dcaf59818cc90846e2bffd88208e88
tagahei <tagahei0505@gmail.com>
2023-11-04 18:48:35 +0900

    定期実行の実装

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
-const { makeReply } = require("./makeReply.js");// 返信生成用の関数を読み込む
-const { howToUseing } = require("./flexmessages/howToUse.js");// 返信生成用の関数を読み込む
+const { makeReply } = require("./makeReply.js"); // 返信生成用の関数を読み込む
+const { howToUseing } = require("./flexmessages/howToUse.js"); // 返信生成用の関数を読み込む
 const express = require("express");
 const line = require("@line/bot-sdk");
 const PORT = process.env.EXPRESS_PORT;
+const { getUserIdList, insertUserId } = require("./database.js");
 
 // env呼び出し
 const config = {
   channelSecret: process.env.LINE_CHANNEL_SECRET,
-  channelAccessToken: process.env.LINE_ACCESS_TOKEN
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
 };
 
 // インスタンス生成
@@ -32,11 +33,16 @@ async function handleEvent(event) {
     mes = await makeReply(event);
   } else if (event.type === "follow") {
     // "follow" イベントの処理
-    mes = { type: "flex", altText: "使い方はこちら！🙂", contents: howToUseing() }; // howToUseing() 関数を呼び出す
+    const userId = event.source.userId; // LINEのユーザーID
+    await insertUserId(userId);
+    mes = {
+      type: "flex",
+      altText: "使い方はこちら！🙂",
+      contents: howToUseing(),
+    }; // howToUseing() 関数を呼び出す
   } else {
     return Promise.resolve(null);
   }
-
 
   // メッセージが空の場合は返信無し
   if (mes == null) {
@@ -51,33 +57,37 @@ async function handleEvent(event) {
 app.listen(PORT);
 // console.log(`Server running at ${PORT}`);
 
-// 
-
-// // cronの設定
-// const cron = require('node-cron');
-// //'秒 分 時 日 月 曜日'
-// cron.schedule('0 0 9 * * *', () => {
-//     console.log("おはよう！朝ご飯、ちゃんと食べた？( ﾟДﾟ)");
-// });
-const cron = require('node-cron');
-//'秒 分 時 日 月 曜日'
-cron.schedule('2 * * * * *', () => {
-    console.log("おはよう！朝ご飯、ちゃんと食べた？( ﾟДﾟ)");
-    // postMessage();
+// 定期実行
+const cron = require("node-cron");
+//朝9時に実行
+cron.schedule("0 0 9 * * *", () => {
+  postMessage();
 });
 
+// //'秒 分 時 日 月 曜日' Debug用1分に1回実行
+// cron.schedule('1 * * * * *', () => {
+//     postMessage();
+// });
 
-async function postMessage(){
-  const userId = "U6a473d6c9ac6194a06f42381e5fd0326"
-  const messages = [{
-    type: 'text',
-    text: "定期実行"
-  }];
-  try {
-    const res = await client.pushMessage(userId, messages);
-    console.log(res);        
-} catch (error) {
-    console.log(`エラー: ${error.statusMessage}`);
-    console.log(error.originalError.response.data);
-}
+const { selectJanalMode } = require("./flexmessages/selectJanalMode.js");
+async function postMessage() {
+  const userIdList = await getUserIdList();
+  const messages = [
+    {
+      type: "text",
+      text: "おはようございます！🌞\n今日もジャーナルで自分の考えを整理しましょう！",
+    },
+    {
+      type: "flex",
+      altText: "ジャーナルサポート",
+      contents: selectJanalMode(),
+    },
+  ];
+  userIdList.map(async (userId) => {
+    try {
+      const res = await client.pushMessage(userId, messages);
+    } catch (error) {
+      console.log(`エラー: ${error}`);
+    }
+  });
 }
