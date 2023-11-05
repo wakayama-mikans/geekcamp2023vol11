1db1b33025f4a04bb991d6c52606efa026cfc7d9
kakeru515 <s246276@wakayama-u.ac.jp>
2023-11-05 13:53:39 +0900

    使い方のフレックスメッセージ変更

--- node/src/flexmessages/howToUse.js
+++ node/src/flexmessages/howToUse.js
@@ -1,63 +1,63 @@
 function howToUseing() {
   return {
-    type: "carousel",
-    contents: [
+    "type": "carousel",
+    "contents": [
       {
-        type: "bubble",
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
             {
-              type: "image",
-              url: "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2F1.png?alt=media&token=8697ee98-9315-4fcf-b1ef-4609e88e9e75",
-              size: "full",
-              aspectMode: "cover",
-              aspectRatio: "1:1",
-              gravity: "top",
-            },
+              "type": "image",
+              "url": "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2Fstart.png?alt=media&token=c80c6c19-88de-4d68-8136-a86521bc8dab",
+              "size": "full",
+              "aspectMode": "cover",
+              "aspectRatio": "1:1.2",
+              "gravity": "top"
+            }
           ],
-          paddingAll: "0px",
-        },
+          "paddingAll": "0px"
+        }
       },
       {
-        type: "bubble",
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
             {
-              type: "image",
-              url: "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2F2.png?alt=media&token=d3a8c160-b22b-4e51-8987-214d2c35ef4f",
-              size: "full",
-              aspectMode: "cover",
-              aspectRatio: "1:1",
-              gravity: "top",
-            },
+              "type": "image",
+              "url": "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2Fview.png?alt=media&token=c0047af8-5ec8-479b-9e4b-1b091d24ba31",
+              "size": "full",
+              "aspectMode": "cover",
+              "aspectRatio": "1:1.2",
+              "gravity": "top"
+            }
           ],
-          paddingAll: "0px",
-        },
+          "paddingAll": "0px"
+        }
       },
       {
-        type: "bubble",
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
             {
-              type: "image",
-              url: "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2F3.png?alt=media&token=b5dc0203-8bd9-483f-894b-f10ccf567509",
-              size: "full",
-              aspectMode: "cover",
-              aspectRatio: "1:1",
-              gravity: "top",
-            },
+              "type": "image",
+              "url": "https://firebasestorage.googleapis.com/v0/b/geekcamp2023vol11.appspot.com/o/how_to_use%2Fmode.png?alt=media&token=1186af7e-20ef-4531-bca7-4d69075588c3",
+              "size": "full",
+              "aspectMode": "cover",
+              "aspectRatio": "1:1.2",
+              "gravity": "top"
+            }
           ],
-          paddingAll: "0px",
-        },
-      },
-    ],
-  };
+          "paddingAll": "0px"
+        }
+      }
+    ]
+  }
 }
 
 module.exports = { howToUseing };
